f656246d6e2853ad7807988fe0f02cc37bd4a325
Carlo Beltrame <carlo@beltra.me>
2019-12-31 16:57:49 +0100

    Clean up

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
-var midi, data;
+const video = document.getElementById('visualization');
+const dataList = document.querySelector('#midi-data ul');
 
 if (navigator.requestMIDIAccess) {
   navigator.requestMIDIAccess({
     sysex: false
   }).then(onMIDISuccess, onMIDIFailure);
 } else {
-  console.warn("No MIDI support in your browser")
+  console.warn("No MIDI support in your browser");
 }
 
-function onMIDISuccess(midiData) {
-  midi = midiData;
+function onMIDIFailure() {
+  console.warn("Not recognising MIDI controller");
+}
+
+function onMIDISuccess(midi) {
   var allInputs = midi.inputs.values();
   for (var input = allInputs.next(); input && !input.done; input = allInputs.next()) {
-    input.value.onmidimessage = gotMIDImessage;
+    input.value.onmidimessage = onMIDIMessage;
   }
 }
-var dataList = document.querySelector('#midi-data ul')
 
-function gotMIDImessage(messageData) {
+function onMIDIMessage(message) {
+  visualizeMIDIMessage(message);
+  // debug
+  addMIDIMessageToList(message);
+}
+
+function visualizeMIDIMessage(message){
+  const data = message.data;
+  const event = data[0];
+  if (event == 144) {
+  // Note on
+  // On some controllers, note off is also in here, with velocity 0
+    const velocity = data[2];
+    video.style.opacity = velocity / 127.0;
+  } else if (event == 128) {
+  // Note off
+    video.style.opacity = 0;
+  }
+}
+
+function addMIDIMessageToList(messageData) {
   if (messageData.data[0] === 248) return;
   var newItem = document.createElement('li');
   newItem.appendChild(document.createTextNode(messageData.data));
   dataList.appendChild(newItem);
-  onMIDImessage(messageData);
-}
-
-function onMIDIFailure() {
-  console.warn("Not recognising MIDI controller")
-}
-
-function onMIDImessage(message){
-  console.log('success');
-  const srcElement = message.srcElement;
-  const data = message.data;
-  const event = data[0];
-  console.log(event);
-  if (event == 144) {
-    const velocity = data[2];
-    console.log(velocity);
-    const video = document.getElementById('myVideo');
-    video.style.opacity = velocity / 127.0;
-  } else if (event == 128) {
-    const video = document.getElementById('myVideo');
-    video.style.opacity = 0;
-  }
 }
